cccbb229196b3cacd1d25c77ea498488b808ad09
tomilone <tonymilone.tech@gmail.com>
2020-06-17 02:41:22 -0800

    fixed config

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres:Atjm1995@localhost/tryit',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://postgres:Atjm1995@localhost/tryit_test',
 };
